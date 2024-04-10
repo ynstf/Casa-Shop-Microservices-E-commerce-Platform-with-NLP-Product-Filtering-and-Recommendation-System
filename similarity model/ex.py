4fa373d4fc2af3dabdef612e92af85e2bfd9ed1f
YONES <107154559+ynstf@users.noreply.github.com>
2024-04-10 11:47:10 +0000

    Delete similarity model directory

--- similarity model/ex.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.keras import backend as K
-from tensorflow.keras.models import load_model
-version = 1
-model_folder = f"model_v{version}"
-max_length = 32
-siamese_lstm_model = load_model(f'{model_folder}/model.h5')
